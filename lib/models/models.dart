655ddf407b0198b16273dddca2315753585a6a65
ArslanAshraf462 <arslan.ashraf.oraxtech@gmail.com>
2023-01-05 19:42:09 +0500

    dev: Added=> the model class

---
+++ lib/models/models.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/foundation.dart' show immutable;
+
+@immutable
+class LoginHandle {
+  final String token;
+
+  const LoginHandle({
+    required this.token,
+  });
+
+  const LoginHandle.fooBar() : token = 'foobar';
+
+  @override
+  bool operator ==(covariant LoginHandle other) => token == other.token;
+
+  @override
+  int get hashCode => super.hashCode;
+
+  @override
+  String toString() => 'LoginHandle (token = $token)';
+}
+
+enum LoginErrors {
+  invalidHandle,
+}
+
+@immutable
+class Notes {
+  final String title;
+
+  const Notes({
+    required this.title,
+  });
+
+  @override
+  String toString() => 'title : $title';
+}
+
+final mockNotes = Iterable.generate(
+  3,
+      (i) => Notes(title: 'Notes $i'),
+);
